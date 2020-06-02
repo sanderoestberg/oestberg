b320055c40c63e6540b23af1e11318310d6680b1
Oskar Wiegaard <oskar.wiegaard@hotmail.com>
2020-06-02 20:00:00 +0200

    kommentar
    kommentar

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import TitleLoop from "./title-loop.js";
 let _titleLoop = new TitleLoop();
 let _spaService = new SpaService("forside");
 
+// now you see it - now you dont
 
 function infLoop(){
     for (let n=0; n < 1000; n++) {
@@ -17,10 +18,16 @@ function infLoop(){
 
 infLoop();
 
+// vores SPA pagechange.
 window.pageChange = function () {
     _spaService.pageChange();
-   
 }
+
+// På vores burger-bar i HTML har vi tilføjet en onClick funtion der bliver udformet her
+// Ved click på burgerbar toggler vi classen "active", der gør vores menu
+// bliver vist frem på siden.
+// samtidigt vil vores burger-btn toggle classen "open" der ændrer fra burger og til et kryds med animationer.
+
 window.bigMac = function() {
    let bar = document.getElementById("burgerbar");
     bar.classList.toggle("active");
--- js/smykker.js
+++ js/smykker.js
@@ -57,6 +57,7 @@ function genreSelected(genreId){
   }
 }
 
+
 function footerSelected(genreName){
   let smykkeOverskrift = document.querySelector('.smykkeh1')
   smykkeOverskrift.innerHTML = genreName
@@ -98,52 +99,6 @@ function appendSmykkerByGenre(smykkerByGenre) {
   document.querySelector('.overview-grid-container').innerHTML = clearGrids + htmlTemplate;
   
 }
-
-  
-  let smykker = [];
-  // Her opretter vi et tomt array med navnet "smykker".
-  // Så initiere vi functionen "get smykkes" hvor vi fetcher alt data om vores recipies som Json data fra vores opskriftdatabase på wordpress.
-  // json og javascript kan frit konverteres frem og tilbage.
-  // Når fetch er færdiggjort afslutter vores spinner.
-  function getSmykker() {
-    fetch('http://oskarwiegaard.dk/wp/wp-json/wp/v2/posts?per_page=100')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(json) {
-        console.log(json);
-        appendSmykker(json);
-        smykker = json;
-        setTimeout(function() {
-          //fjerner spinner efter load.
-          showLoader(false);
-        }, 200);
-      });
-  }
-  
-  // getSmykker();
-  
-  
-  
-  // Da vores json er fetchet kan vi nu append det til DOM.
-  // vi initiere herefter et for-loop hvor vi kører gennem den samme kodeblok igen og igen inditl alle smykkerne er gennemgået.
-  // noget af dataen er gemt væk under "moreInfo" classen som bliver vist ved at trykke på objectet på siden.
-  function appendSmykker(smykker) {
-    let htmlTemplate = "";
-  
-    for (let smykke of smykker) {
-      htmlTemplate += `
-          <img src="${smykke.acf.smallimg}" onclick="appendSmykkerBig('${smykke.acf.bigimg}', '${smykke.acf.info}', '${smykke.acf.pris}')">
-      `;
-    }
-  
-  
-  //Smykkerne bliver hermed appended til innerHTML i vores container ".overview-grid-container"
-  
-    document.querySelector('.overview-grid-container').innerHTML += htmlTemplate;
-  
-  
-  }
 
 
   function appendSmykkerBig(bigimg, info, pris) {
