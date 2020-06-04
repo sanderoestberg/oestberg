3b2654d6b8ded49a53ef200c7ac9e3658241b6fc
sanderoestberg <47508252+sanderoestberg@users.noreply.github.com>
2020-06-04 05:15:48 +0200

    Update smykker.js

--- js/smykker.js
+++ js/smykker.js
@@ -3,26 +3,26 @@
 // showLoader(false) = loader bliver skjult
 
 function showLoader(show) {
-    let loader = document.querySelector('#loader');
-    if (show) {
-      loader.classList.remove("hide");
-    } else {
-      loader.classList.add("hide");
-    }
+  let loader = document.querySelector('#loader');
+  if (show) {
+    loader.classList.remove("hide");
+  } else {
+    loader.classList.add("hide");
   }
-  
-  
+}
 
- // Her fetcher vi alle genrene fra vores WordPress Headless CMS
- // Vi henter alle kategorierne inden for parent-kategorien: smykker(id=12)
- // Respone = Array(6): Armbånd, Broche, Halskæde, Ring, Øreclips, Øreringe
- // Herefter kalder vi functionen appendGenres(categories), med arrayet som parameter.
+
+
+// Her fetcher vi alle genrene fra vores WordPress Headless CMS
+// Vi henter alle kategorierne inden for parent-kategorien: smykker(id=12)
+// Respone = Array(6): Armbånd, Broche, Halskæde, Ring, Øreclips, Øreringe
+// Herefter kalder vi functionen appendGenres(categories), med arrayet som parameter.
 function getGenres() {
   fetch('http://oskarwiegaard.dk/wp/wp-json/wp/v2/categories?parent=12')
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(categories) {
+    .then(function (categories) {
       console.log(categories);
       appendGenres(categories);
       showLoader(false);
@@ -55,15 +55,15 @@ function appendGenres(genres) {
 // med genre.id'et som parameter for den valgt kategori
 // Herefter fetcher vi alle posts inden for den valgte kategori ved hjælp af &categories=${genreId}
 // Til sidst bliver appendSmykkerByGenre(smykker) kaldt.
-function genreSelected(genreId){
+function genreSelected(genreId) {
   console.log(`Genre ID: ${genreId}`);
   if (genreId) {
     showLoader(true);
     fetch(`http://oskarwiegaard.dk/wp/wp-json/wp/v2/posts?_embed&categories=${genreId}&per_page=100`)
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
       })
-      .then(function(smykker) {
+      .then(function (smykker) {
         console.log(smykker);
         appendSmykkerByGenre(smykker);
         showLoader(false);
@@ -73,7 +73,7 @@ function genreSelected(genreId){
 
 // Udover at genreSelected som onclick, har vi også disse to functioner: footerSelected(genreName) og selected(genreName, element)
 // footerSelected bruges til at append overskrift navnet til den valgt menu på smykke-oversigt siden.
-function footerSelected(genreName){
+function footerSelected(genreName) {
   let smykkeOverskrift = document.querySelector('.smykkeh1')
   smykkeOverskrift.innerHTML = genreName
 }
@@ -81,21 +81,17 @@ function footerSelected(genreName){
 // men derudover bliver der også tilføjet classen 'highlighted' som giver det valgte menupunkt en gul streg og tykkere skrift
 // for-of loopet er til for at fjerne classen highlight fra andre menupunkter når en ny kategori bliver valgt
 
-function selected(genreName, element){
+function selected(genreName, element) {
   let menuItems = document.querySelectorAll('#smykke-menu-items a li')
   let smykkeOverskrift = document.querySelector('.smykkeh1')
-  console.log(genreName)
   smykkeOverskrift.innerHTML = genreName
-  console.log(menuItems)
-  console.log(element)
-
   for (let menuItem of menuItems) {
-  if(menuItem.classList.contains("highlighted")){
-    menuItem.classList.remove("highlighted")
+    if (menuItem.classList.contains("highlighted")) {
+      menuItem.classList.remove("highlighted")
+    } else {
+      element.classList.add('highlighted')
+    }
   }
-  else{
-    element.classList.add('highlighted')}
-}
 }
 
 // funktionen her bliver kaldt fra genreSelected(genreId) og bruges til at tilføje de posts der er i den valgte kategori
@@ -117,29 +113,28 @@ function appendSmykkerByGenre(smykkerByGenre) {
     `;
     appendSmykkerBig(`${smykke.acf.bigimg}`, `${smykke.acf.info}`, `${smykke.acf.pris}`)
   }
- 
+
   document.querySelector('.overview-grid-container').innerHTML = clearGrids + htmlTemplate;
-  
+
 }
 
 // Bliver kaldt når brugeren klikker på det lille billede, og for herved vidst et større preview billede samt info-teskt og pris
-  function appendSmykkerBig(bigimg, info, pris) {
-    console.log(bigimg, info, pris);
-    let htmlTemplate = "";
-  
-      htmlTemplate += `
+function appendSmykkerBig(bigimg, info, pris) {
+  console.log(bigimg, info, pris);
+  let htmlTemplate = "";
+
+  htmlTemplate += `
           <div id="big-img-preview">
           <img src="${bigimg}">
           <p>${info}</p>
           <h4>${pris} kr.</h4>
           </div>
       `;
-  
-  
+
+
   //Smykkerne bliver hermed appended til innerHTML i vores container ".left-grid"
-  
-    document.querySelector('.left-grid').innerHTML = htmlTemplate;
-  
-  
-  }
-  
+
+  document.querySelector('.left-grid').innerHTML = htmlTemplate;
+
+
+}
